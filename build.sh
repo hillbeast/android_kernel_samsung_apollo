471ba0d390735fd7d918c36bec8e896aefc2be23
Mark Kennard <komcomputers@gmail.com>
2012-05-07 13:29:19 +1200

    Adjust initramfs location

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Generating initramfs.cpio"
-cd ../initramfs
+cd initramfs
 find . -print0 | cpio --null -ov --format=newc > ../initramfs.cpio
-cd ../Kernel
+cd ..
 
 if [ -z $1 ]; then
 	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
